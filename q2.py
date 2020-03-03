8520a19c1c7c6fd329ceedc06e39747c49147e01
DanJamRod <rodgersd09@gmail.com>
2020-03-03 17:35:17 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -7,23 +7,25 @@ def drunkard_walk(x, y, n):
 
     return the Manhattan distance after n intersections(steps) from the origin
     """
-    for i in range(n):
-        a = random.randint(1,4)
-        if a == 1:
-            x += -1
-        elif a == 2:
-            y += -1
-        elif a==3:
-            x += 1 
-        else:
-            y += 1
-    return abs(x) + abs(y)           
+    x_0 = x # Starting coordinatesn defined as x_0 and y_0 so not forgotten
+    y_0 = y
+    for i in range(n): # The program repeats "n" times to simulate the drunkard taking "n" steps 
+        a = random.randint(1,4) # 4 possible directions, N, S, E, W -> Chooses one at random
+        if a == 1: # 1 in 4 chance
+            x += -1 # Simulate a step West
+        elif a == 2: # 1 in 4 chance
+            y += -1 # Simulate a step South
+        elif a==3: # 1 in 4 chance
+            x += 1 # Simulate a East
+        else: # 1 in 4 chance
+            y += 1 # Simulate a step North
+    return abs(x_0 - x) + abs(y_0 - y) # Returns blocks North or South added to blocks East or West (manhattan distance)       
 
 
 def main():
-    x_0 = 0
-    y_0 = 0
-    steps = 100
+    x_0 = 3 # Simulate starting at x = 3
+    y_0 = -5 # Simulate starting at y = -5
+    steps = 100 # Simulate 100 steps
     print(f"The drunkard started from ({x_0}, {y_0}).")
     distance = drunkard_walk(x_0, y_0, steps)
     print(f"After {steps} intersections, he's {distance} blocks from where he started.")
